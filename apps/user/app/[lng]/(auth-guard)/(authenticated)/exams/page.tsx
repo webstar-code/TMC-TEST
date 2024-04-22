2f9f0d6747e62d50ba2342d668ea5f35b57a38b8
webstar-code <bhaveshrc2001@gmail.com>
2024-04-22 19:16:29 +0530

    feat: profile review UI

--- apps/user/app/[lng]/(auth-guard)/(authenticated)/exams/page.tsx
+++ apps/user/app/[lng]/(auth-guard)/(authenticated)/exams/page.tsx
@@ -2,6 +2,13 @@
 
 import { useUserStore } from "lib/store";
 import Link from "next/link";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+  Button,
+} from "ui";
 import { ROUTES } from "utils/routes";
 
 export default function ExamsPage() {
@@ -16,7 +23,59 @@ export default function ExamsPage() {
           <h1 className="text-2xl font-semibold">Exam history</h1>
         </div>
 
-        <div className="w-full flex justify-between items-end p-6 rounded-lg bg-white drop-shadow-lg">
+        <div className="w-full min-h-[256px] bg-white rounded-lg shadow-lg flex flex-col items-center justify-center gap-6 p-6">
+          <h1 className="text-xl font-semibold">
+            Your profile is under review!
+          </h1>
+          <p className="text-[#868686] max-w-md text-center">
+            Your approval will be processed within 3-7 business days. If you
+            have any further questions, please contact support.
+          </p>
+          <Button>Contact Support</Button>
+        </div>
+
+        {/* <div className="w-full min-h-[256px] bg-white rounded-lg shadow-lg flex flex-col items-center justify-center gap-6 p-6">
+          <h1 className="text-xl font-semibold">Your application has been rejected</h1>
+          <p className="text-[#868686] max-w-md">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <Button>Contact Support</Button>
+        </div> */}
+
+        <div className="w-full flex flex-col">
+          <h1 className="text-xl font-semibold mb-6">
+            Why your profile might be under review:
+          </h1>
+          <Accordion type="single" collapsible>
+            <AccordionItem value="item-1">
+              <AccordionTrigger>Identity verification</AccordionTrigger>
+              <AccordionContent>
+                <div className="font-normal flex flex-col gap-1 text-base">
+                  We must ensure your identity and all health information aligns
+                  to ensure privacy and security.
+                </div>
+              </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-2">
+              <AccordionTrigger>Review for duplicate accounts</AccordionTrigger>
+              <AccordionContent>
+                <div className="font-normal flex flex-col gap-1 text-base">
+                  We must ensure your identity and all health information aligns
+                  to ensure privacy and security.
+                </div>
+              </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-3">
+              <AccordionTrigger>Incorrect information</AccordionTrigger>
+              <AccordionContent>
+                <div className="font-normal flex flex-col gap-1 text-base">
+                  We must ensure your identity and all health information aligns
+                  to ensure privacy and security.
+                </div>
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        </div>
+
+        {/* <div className="w-full flex justify-between items-end p-6 rounded-lg bg-white drop-shadow-lg">
           <div className="flex flex-col gap-4">
             <h1 className="text-xl font-semibold">
               Subscribe to start using Track My Care!
@@ -31,7 +90,7 @@ export default function ExamsPage() {
               View Plans
             </p>
           </Link>
-        </div>
+        </div> */}
       </div>
     </div>
   );
--- apps/user/app/[lng]/layout.tsx
+++ apps/user/app/[lng]/layout.tsx
@@ -1,4 +1,3 @@
-import "../../styles/globals.css";
 import "ui/globals.css";
 import "../../styles/globals.css";
 import type { Metadata } from "next";
