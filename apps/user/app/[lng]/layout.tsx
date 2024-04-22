f2cedd8509db336ce4b7acc050f0788de17e60bb
webstar-code <bhaveshrc2001@gmail.com>
2024-04-22 16:58:10 +0530

    fix: add viewport meta

--- apps/user/app/[lng]/layout.tsx
+++ apps/user/app/[lng]/layout.tsx
@@ -1,17 +1,21 @@
 import "ui/globals.css";
+import "../../styles/globals.css";
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans, Space_Grotesk } from "next/font/google";
-import "../../styles/globals.css";
 import { dir } from "i18next";
 import { languages } from "../i18n/settings";
 import { Toaster } from "ui";
-
+import type { Viewport } from "next";
 const plusJakartaSans = Plus_Jakarta_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Track My Care",
   description:
     "Access Your Health Records Anywhere, Anytime with Track My Care!",
+};
+export const viewport: Viewport = {
+  initialScale: 1,
+  width: "device-width",
 };
 
 export async function generateStaticParams() {
