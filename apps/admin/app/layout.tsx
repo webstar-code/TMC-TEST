19001d7b3a0d92eb3d751834cdd71e1ee5a9551f
webstar-code <bhaveshrc2001@gmail.com>
2024-04-22 20:28:07 +0530

    fix: admin config fix

--- apps/admin/app/layout.tsx
+++ apps/admin/app/layout.tsx
@@ -1,5 +1,5 @@
 import "ui/globals.css";
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 
@@ -9,6 +9,10 @@ export const metadata: Metadata = {
   title: "Admin - Track My Care",
   description:
     "Access Your Health Records Anywhere, Anytime with Track My Care!",
+};
+export const viewport: Viewport = {
+  initialScale: 1,
+  width: "device-width",
 };
 
 export default function RootLayout({
